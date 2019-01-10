2d095c9f1ec958db6f0d08490bc936246ebbb7ac
O'Toole <WBO5@pitt.edu>
2019-01-10 16:32:06 -0500

    minor error fix

--- Assets/Code/Mechanics/Navigation/NavigationAgent.cs
+++ Assets/Code/Mechanics/Navigation/NavigationAgent.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-[RequireComponent(typeof(NavMeshAgent)]
+[RequireComponent(typeof(NavMeshAgent))]
 public class NavigationAgent : MonoBehaviour
 {
     #region Fields and Properties
